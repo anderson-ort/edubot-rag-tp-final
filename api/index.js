f8ef306eb073905c3a73b543e2114239f35ad308
andru-ort <anderson.ocana@ort.edu.ar>
2026-06-03 21:58:44 -0300

    update verccel deploy

---
+++ api/index.js
@@ -0,0 +1,18 @@
+import app from "../app.js";
+import config from "../src/config/config.js";
+import { connectMongo } from "../src/databases/mongo.cnx";
+
+let isConnected = false;
+
+export default async function handler(req, res) {
+    if (config.chatStorage === "mongo" && !isConnected) {
+        try {
+            await connectMongo();
+            isConnected = true;
+            console.log("Connected to MongoDB");
+        } catch (error) {
+            console.error("Failed to connect to MongoDB:", error);
+        }
+    }
+    return app(req, res);
+}
